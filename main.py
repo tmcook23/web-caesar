01a7e0d7ec1ae3a48d44fe53f1cc663394fed259
Tracy Cook <tracymcook23@gmail.com>
2019-04-01 22:53:19 -0500

    Render a form

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
+# Tracy Cook
+# LC101
+# April 1, 2019
+
+
 from flask import Flask
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <!-- create your form here -->
+      <form method="post">
+        <label>
+            Rotate by:
+            <input type="text" name="rot" value="0"/>   
+        </label>
+        <textarea name="text" rows = "5" cols = "20"> </textarea>
+        <input type="submit" value="Submit Query">
+    </form>
+    </body>
+</html>
+"""
+
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
 
 app.run()
