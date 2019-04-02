c80ca79e21b28e948a39dd9d710f2a2c61560611
Tracy Cook <tracymcook23@gmail.com>
2019-04-01 23:30:34 -0500

    Render the form after encryption

--- main.py
+++ main.py
@@ -15,19 +15,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -37,7 +37,7 @@ form = """
             Rotate by:
             <input type="text" name="rot" value="0"/>   
         </label>
-        <textarea name="text" rows = "5" cols = "20"> </textarea>
+        <textarea name="text" rows = "5" cols = "20">{0}</textarea>
         <input type="submit" value="Submit Query">
     </form>
     </body>
@@ -50,10 +50,12 @@ def encrypt():
     text = str(request.form['text'])
 
     result = rotate_string(text, rot)
-    return "<h1>" + result + "</h1>"
+    #return "<h1>" + result + "</h1>"
+    return form.format(result)
     
 @app.route("/")
 def index():
-    return form
+    #return form
+    return form.format(" ")
 
 app.run()
